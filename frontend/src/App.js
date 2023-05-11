c2f46cc52699ae4e2dc185abcec0c72739af5a84
Szymon <szwielgosz259@gmail.com>
2023-05-11 18:12:47 +0200

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import RemindPassowrdPage from "./pages/RemindPasswordPage";
+
+import styles from './styles/app.module.scss'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <RemindPassowrdPage/>
+    </div>
+  );
+}
+
+export default App;
